91f1a1794b751d6f9f20ce4a76323507e70f8e88
zatatech <zatatech@gmail.com>
2022-08-17 09:16:19 -0400

    modificaciones DOTENV

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT || 4500
 
 app.get('/', (req, res) => {
     console.log('hoola')
-    res.send('holass')
+    res.send('Primer Get')
 })
 
-app.listen(4500,() => { 
-    console.log("hola en el puerto 4500")
+
+app.listen(PORT,() => { 
+    console.log(`hola en el puerto ${PORT} `)
 })
 
 
