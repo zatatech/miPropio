932f1411c0810181ed07b99f663238dee73664ae
zatatech <zatatech@gmail.com>
2022-08-22 12:26:35 -0400

    hasta la creacion de Style.css

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require("path")
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 const PORT = process.env.PORT || 4500
 
-app.get('/', (req, res) => {
-    console.log('hoola')
-    res.send('Primer Get')
-})
+//MIDDLEWARES
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 
 app.listen(PORT,() => { 
